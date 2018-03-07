9052d5a2bfb070edff274381db436415a53b4800
SAnjiHolic <sanhah0322@naver.com>
2018-03-07 11:26:06 +0900

    20180307

--- Search_used_modules/Module_search.pl
+++ Search_used_modules/Module_search.pl
@@ -1,64 +1,62 @@
-my @modules; 
+my @now_mod; 
 my @inc = &inc;
-$check_flag = 0;
 $aad = 0;
-@now_mod;
-$now_mod;
+$level = -1; 
 @tt = split(/\\/,$ARGV[0]);
 @tt = split(/\//,$ARGV[0]) unless $tt[0];
-$now_mod = $tt[-1];
-print $now_mod."\n";
 
-modules_name($ARGV[0]);
+modules_name($ARGV[0],$tt[-1]);
 
 sub modules_name{
+	$level++;
 	my $name = shift;
-	my $name2 = shift;
-	my @tra = split "/",$name;
-	my $name_real = $tra[-1];
-	unshift(@now_mod,$name_real);
-	$now_mod = $now_mod[0];
-	$now_mod = $tt[-1] unless ($now_mod);
+	my $name_real = shift;
+	$now_mod[$level] = $name_real;
+	$now_mod = $name_real;
 	open (F,$name);	my @files = <F>; close(F);
 	foreach (@files) {
-		if(/^use /){
-			s/^use | |;|\n|qw.*|\(.*//g;
-			my @tmp = split " "; next if ($tmp[3]);		
-			#unless (/strict/|/warnings/|/vars/|/(constantDEBUG)+/){ 
-			unless (/strict/|/warnings/|/vars/|/(5\.)+/|/constant/|/Encode/) {				
-			push(@modules,$_);	$aad++;
-			modules_find($modules[-1],$now_mod);
+			if(/^use /){
+				s/^use | |;|\n|qw.*|\(.*//g;
+				my @tmp = split " "; next if ($tmp[3]);	
+				unless (/strict/|/warnings/|/vars/|/(5\.)+/|/constant/) {				
+				$aad++;
+				modules_find($_,$now_mod)
+				}			
+			elsif(/__END__/){
+				if(/^__END__/){last}
+				elsif(/(^ )+__END__/){last}
+					}
+				}
 			}
-			}
-		elsif(/__END__/ ){last;}
-	}
-	$now_mod = $tt[-1];
+	$level--;
+	$now_mod = $now_mod[$level];
 }
 
 sub modules_find{ 
 	my $mod_name = $_ if ($_);
+	my $brk;
 	print "$aad : use ";
 	print $mod_name if ($_);
 	print " \($now_mod => $mod_name) " if ($now_mod);
 	print "\n";
 	s/::/\//g;	
 	$pm_flag = substr $mod_name, -2;
-	#$pm_flag = substr $mod_name, -2;
 	$mod_name .=".pm" unless ($mod_name eq "pm");		
 	foreach (@inc) {
 		$where = $_.$mod_name;
-		#print $where." ";
-		#print "$mod_name\n" if ($_);
 		if (-f $where) {
-			modules_name($where,$_);
+			$brk = 1;
+			modules_name($where,$mod_name);
 			}
-		elsif ($check_flag) {last;}
-		else {last;}
+		elsif($brk){$brk = 0; last}
+		else {last}
 	}
 }
 sub inc{
-	$flag; $number;	@lib;
-	@a = `perl -V`;	
+	my $flag; 
+	my $number; 
+	my @lib;
+	my @a = `perl -V`;	
 	foreach (@a) {
 		if ($flag) {
 			s/ |\n//g;
